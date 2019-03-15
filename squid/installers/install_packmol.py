ddcffb34e4f7e7f114829f9fe3a6eb121de82a7b
Henry Herbol <hherbol@gmail.com>
2019-03-15 11:51:31 -0400

    Fix to packmol install for marcc

--- squid/installers/install_packmol.py
+++ squid/installers/install_packmol.py
@@ -15,6 +15,18 @@ def run_install(location, MODULEDIR):
         os.system("git clone https://github.com/mcubeg/packmol")
         os.chdir("packmol")
         os.system("./configure")
+
+        BAD_MARCC_FORTRAN = "FORTRAN=/software/centos7/bin/gfortran"
+        GOOD_MARCC_FORTRAN = "FORTRAN=/software/apps/compilers/gcc/6.4.0/bin/gfortran"
+        # On MARCC, if the FORTRAN attempted to use is BAD_MARCC_FOTRAN, then
+        # switch to GOOD_MARCC_FORTRAN
+        makefile = open("Makefile", 'r').read()
+        if BAD_MARCC_FORTRAN in makefile:
+            makefile = makefile.replace(BAD_MARCC_FORTRAN, GOOD_MARCC_FORTRAN)
+            fptr = open("Makefile", 'w')
+            fptr.write(makefile)
+            fptr.close()
+
         os.system("make")
         os.chdir("../")
 
