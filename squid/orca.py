49d5a71b019067b753c153135156846b8bea5a3b
Henry C. Herbol <hherbol@gmail.com>
2019-04-26 10:51:22 -0400

    Forgot strings in formatted strings

--- squid/orca.py
+++ squid/orca.py
@@ -556,7 +556,7 @@ def jobarray(run_name, route, frames, extra_section='', grad=False,
         additional_env_vars=orca_env,
         sub_flag=sysconst.orca_sub_flag, slurm_allocation=slurm_allocation,
         jobarray=(0, len(frames) - 1),
-        outfile_name="orca/%s.%a/%s.%a.o%j"
+        outfile_name="orca/%s.%a/%s.%a.o%j" % (run_name, run_name)
     )
 
 
