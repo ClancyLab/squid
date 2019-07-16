fb08b44742fed8a5f4dcab9709ba4967b2e014a8
Henry C. Herbol <hherbol@gmail.com>
2019-07-16 17:51:54 -0400

    Changed version

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+description = "A set of modules to aid in atomistic and molecular simulations."
+long_description = open("README.rst", 'r').read().strip()
+
+setuptools.setup(
+    name='squid',
+    version='2.0.0',
+    author="Clancy Group",
+    author_email="ClancyLabJHU@gmail.com",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/rst",
+    url="https://github.com/clancylab/squid",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC BY-NC 3.0 US",
+        "Operating System :: OS Independent",
+    ],
+)
