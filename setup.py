d4e3c7ca43b0961a14e0682b75711c3adb39ed00
Henry C. Herbol <hherbol@gmail.com>
2019-08-05 17:04:58 -0400

    Updated wheel

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.rst", 'r').read().strip()
 
 setuptools.setup(
     name='clancyLab-squid',
-    version='2.0.18',
+    version='2.0.19',
     author="Clancy Group",
     author_email="ClancyLabJHU@gmail.com",
     description=description,
