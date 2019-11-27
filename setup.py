6bdddc122c890ed47ff01c81ff1c9e578ed3edfe
Henry C. Herbol <hherbol@gmail.com>
2019-11-27 14:03:31 -0500

    Squid wheel

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.rst", 'r').read().strip()
 
 setuptools.setup(
     name='clancyLab-squid',
-    version='2.0.42',
+    version='2.1.0',
     author="Clancy Group",
     author_email="ClancyLabJHU@gmail.com",
     description=description,
