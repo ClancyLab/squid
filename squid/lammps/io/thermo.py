f40599834fef79e142e04bb2bc395693170882a3
Divya <ds2299@cornell.edu>
2019-12-12 12:45:29 -0500

    Test read thermo

--- squid/lammps/io/thermo.py
+++ squid/lammps/io/thermo.py
@@ -189,3 +189,19 @@
 #             str += '"' + word + '",'
 #         str = str[:-1] + ')'
 #         eval(str)
+
+def read_log_or_out(fileObj, thermo_output_list):
+
+    flag = False
+    for line in fileObj:
+        data = line.split()
+        try:
+            if data[0] == 'Loop':
+                flag = False
+            if flag:
+                yield data
+            if data == thermo_output_list:
+                flag = True
+        except (IndexError, ValueError):
+            pass
+
